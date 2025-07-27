14377812fa83bfbf5b21e2ebe97388053fa48b89
Amirali Mollaei <me.amiralimollaei@gmail.com>
2025-07-27 16:40:34 +0330

    Refactor AsyncCommandManager for better readability

--- pixi/commands.py
+++ pixi/commands.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import AsyncIterator, Generator
 
 from .typing import AsyncFunction, Optional, Iterator, AsyncGenerator
 from .chatting import ChatMessage
@@ -48,10 +49,31 @@ class AsyncCommandManager:
 
     def get_prompt(self):
         return "\n".join(["- "+func.get_syntax() for name, func in self.commands.items()])
+    
+    async def execute_command(self, command_str: str, reference_message: ChatMessage | None = None):
+        command_content = command_str[1:-1]
+        seperator_idx = None
+        if ":" in command_str:
+            seperator_idx = command_content.index(":")
 
-    # consumes commands and runs them automatically
-    async def stream_commands(self, stream: Iterator | AsyncGenerator, refrence_message: ChatMessage):
-        inside_command = 0
+        if seperator_idx:
+            command_name = command_content[:seperator_idx].strip()
+            command_data = command_content[seperator_idx+1:].strip()
+        else:
+            command_name = command_content
+            command_data = None
+
+        maybe_command_fn = self.commands.get(command_name.lower())
+        if maybe_command_fn is None:
+            raise NotImplementedError(f"The command `{command_name}` is not implemented.")
+
+        return await maybe_command_fn(reference_message, command_data)
+    
+    async def stream_commands(self, stream: Iterator | Generator | AsyncGenerator | AsyncGenerator, reference_message: ChatMessage):
+        """
+        Consumes commands and runs them automatically
+        """
+        inside_command = 0 # counts the number of "[" characters minus the number of "]" characters
         command_str = ""
 
         async def process(char):
@@ -60,49 +82,38 @@ class AsyncCommandManager:
 
             result = None
 
+            # the opening of the command
             if char == "[":
                 inside_command += 1
-
+            
             if inside_command != 0:
                 command_str += char
             else:
                 result = char
 
+            # the closing of the command
             if char == "]":
-                inside_command -= 1
+                inside_command -= 1 
 
+                # if the command is fully captured
                 if inside_command == 0:
-                    _command_str = command_str[1:-1]
-                    seperator_idx = None
-                    if ":" in command_str:
-                        seperator_idx = _command_str.index(":")
-
-                    command_data = None
-                    if seperator_idx:
-                        command_name = _command_str[:seperator_idx].strip()
-                        command_data = _command_str[seperator_idx+1:].strip()
-                    else:
-                        command_name = _command_str
-
-                    command = self.commands.get(command_name.lower())
-                    if command is not None:
-                        await command(refrence_message, command_data)
-                    else:
-                        raise NotImplementedError(f"The command `{command_name}` is not implemented.")
-
+                    await self.execute_command(
+                        command_str=command_str,
+                        reference_message=reference_message
+                    )
                     command_str = ""
 
             return result
 
-        if isinstance(stream, Iterator):
+        if isinstance(stream, (Iterator, Generator)):
             for char in stream:
                 result = await process(char)
                 if result:
                     yield result
-        elif isinstance(stream, AsyncGenerator):
+        elif isinstance(stream, (AsyncIterator, AsyncGenerator)):
             async for char in stream:
                 result = await process(char)
                 if result:
                     yield result
         else:
-            raise TypeError(f"expected `stream` to be an Iterator or an AsyncGenerator but got `{type(stream)}`!")
+            raise TypeError(f"expected `stream` to be an Iterator, Generator, AsyncIterator or AsyncGenerator but got `{type(stream)}`!")
